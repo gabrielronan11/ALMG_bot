a2f77d72580baa6d9b6553563e0a8f0cf0c244f3
gabrielronan11 <114266038+gabrielronan11@users.noreply.github.com>
2023-04-04 21:19:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import os
+import gspread
+import requests
+from flask import Flask, request
+from oauth2client.service_account import ServiceAccountCredentials
+import datetime
+import pandas as pd
+import json
+from bs4 import BeautifulSoup
 
