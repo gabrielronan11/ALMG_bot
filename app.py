468dcd27e9b44571dc1fcdc5380cb05ca8cefe48
gabrielronan11 <114266038+gabrielronan11@users.noreply.github.com>
2023-04-05 11:48:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,7 @@ api = gspread.authorize(conta)
 planilha = api.open_by_key("1mOd6Muax8S58sSTbH68e-i8rQm8JEyYbDZnZowDrEzY")
 sheet = planilha.worksheet("Página1")
 app = Flask(__name__)
-df_agenda = None
-@app.route("/dataframe-ALMG")
 def dataframe_ALMG():
-  global df_agenda
   site_almg = requests.get('https://www.almg.gov.br/')
   bs = BeautifulSoup(site_almg.content)
   agenda_items = bs.findAll('div', {'class': 'almg-css_eventoDescTit'})
@@ -34,7 +31,7 @@ def dataframe_ALMG():
     local = bs.findAll('p', {"class": "m-0 mt-2 text-gray-550"})[i].text
     agenda_dia.append([comissão, link, horario, local])
   df_agenda = pd.DataFrame(agenda_dia, columns=['Comissão', 'Link da Agenda', 'Horário', 'Local'])
-  return "Ok!"
+  return df_agenda
 
 @app.route("/telegram-bot", methods=["POST"])
 def telegram_bot():
@@ -44,11 +41,8 @@ def telegram_bot():
   if message == "/start":
     texto_resposta=  emoji.emojize(":robot:") + emoji.emojize(":clapping_hands:") + emoji.emojize(":robot:") + "\n\n" + "Olá! Seja bem-vindo(a)! Esse é um bot no qual você pode ver a agenda da Assembleia Legislativa em Minas Gerais no dia de hoje. \n\n Digite: '<b>Qual a agenda da ALMG hoje?</b>' e confira!"
   elif message == "Qual a agenda da ALMG hoje?":
-    if df_agenda is None:
-      texto_resposta = "A agenda da ALMG ainda não foi atualizada. Tente novamente mais tarde."
-    else:
-      texto_resposta = emoji.emojize(":police_car_light:") + "<b>ESSA É A AGENDA DA ALMG HOJE</b>" + emoji.emojize(":police_car_light:") + "\n\n" 
-      for i, row in df_agenda.iterrows():
+    texto_resposta = emoji.emojize(":police_car_light:") + "<b>ESSA É A AGENDA DA ALMG HOJE</b>" + emoji.emojize(":police_car_light:") + "\n\n" 
+    for i, row in dataframe_ALMG().iterrows():
         texto_resposta += emoji.emojize(":pushpin:") + f"{row['Comissão']}\n" + emoji.emojize(":alarm_clock:") + f"{row['Horário']}\n" + emoji.emojize(":house:") + f"{row['Local']}\n" + emoji.emojize(":link:") + f"Mais informações em: {row['Link da Agenda']}\n\n"
   else:
     texto_resposta = "Não entendi!" + emoji.emojize(":sad_but_relieved_face:") + "\n" + "Se você quer saber a agenda da Assembleia Legislativa de Minas Gerais hoje, pergunte: \n <b>'Qual a agenda da ALMG hoje?'</b>"
